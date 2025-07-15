fa9c7f53a9cbf58cf78b884a2460a382ebd6c674
Ritesh Sargam <113118850+riteshsargam@users.noreply.github.com>
2025-07-15 21:37:05 +0530

    Star.cs

---
+++ Star.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using System;
+class Program {
+   static void Main() {
+      int i, j, k;
+      int n = 6,
+      // loop to print the series
+      for (i = 1; i <= n; i++) {
+         for (j = 1; j <= n - i; j++) {
+               Console.Write(" ");
+         }
+         for (k = 1; k <= i; k++) {
+            Console.Write("*");
+         }
+         Console.WriteLine("");
+      }
+      Console.ReadLine();
+   }
+}
